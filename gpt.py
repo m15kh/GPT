2bd6aaf7c4e9605d6d89419786ec933cd0eacd29
m15kh <mohammad.khalili1515@gmail.com>
2025-02-16 19:54:55 +0000

    start

--- gpt.py
+++ gpt.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "api-key"
 
 import openai
 response = openai.chat.completions.create(
